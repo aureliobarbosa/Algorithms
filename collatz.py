59e9166487e1a9ba4ce948e5182248f990c9a99a
Marco Barbosa <aureliobarbosa@gmail.com>
2024-06-13 07:31:44 -0300

    comments on how to improve the collatz code

--- collatz.py
+++ collatz.py
@@ -1,11 +1,18 @@
+# Algumas pequenas sugestões.
+# Para alcançar números maiores seria interessante usar numpy.
+
+
+# the simplest way to speedup this is to use make this  function a lookup table
 def is_prime(num):
     if num < 2:
         return False
-    for i in range(2, int(num ** 0.5) + 1):
+    for i in range(2, int(num**0.5) + 1):
         if num % i == 0:
             return False
     return True
 
+
+# By using recursion it is possible to calculate them in a faster way. Check my Prime decomposition.
 def prime_factors(num):
     factors = []
     divisor = 2
@@ -18,15 +25,14 @@ def prime_factors(num):
     return factors
 
 
-
-
+# Heheh, o ideal é não usar os prints, talvez guardar o histórico com a
 def collatz(n):
     numeros = []
     ind = []
-    i=1
+    i = 1
     numeros.append(n)
     ind.append(i)
-    nu_impar=0
+    nu_impar = 0
     nu_par = 0
     print("Número:", n)
     print("Fatores primos:", prime_factors(n))
@@ -41,13 +47,12 @@ def collatz(n):
         print("Número:", n)
         print("Fatores primos:", prime_factors(n))
         print()
-        i=i+1
+        i = i + 1
         numeros.append(n)
         ind.append(i)
-        
 
-    print('Total impares ', nu_impar)
-    print('Total pares ' , nu_par)
-    print(' Total operacoes ', nu_par+nu_impar)
-    print(' Indice ', ind)
-    print(' Numeros ', numeros)
+    print("Total impares ", nu_impar)
+    print("Total pares ", nu_par)
+    print(" Total operacoes ", nu_par + nu_impar)
+    print(" Indice ", ind)
+    print(" Numeros ", numeros)
